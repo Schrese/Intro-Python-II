edf13116371e95276848c28a8915b6a89fdd3a49
Schrese <53787633+Schrese@users.noreply.github.com>
2020-06-03 18:06:52 -0400

    Trying out directions in the way I know from lua This might need to change later, though

--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = 'empty'
+        self.s_to = 'empty'
+        self.e_to = 'empty'
+        self.w_to = 'empty'
