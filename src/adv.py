bad36304edacbc8da6872a7072d8a1be4d9f98cb
Schrese <53787633+Schrese@users.noreply.github.com>
2020-06-03 20:47:54 -0400

    Finally got things working with conditionals, Tomorrow will try to get to work more programatically

--- src/adv.py
+++ src/adv.py
@@ -63,7 +63,10 @@ room['treasure'].s_to = room['narrow']
 # print(player1.current_room)
 # print(room["outside"].name)
 
-
+# for i in room:
+#     # print(dir(i[0]))
+#     print(i)
+ 
 
 
 
@@ -84,6 +87,8 @@ print("Are you ready? ")
 print("GOOD!")
 print("remember, you can always quit the game by entering 'q'")
 
+empty = 'empty' # only uses one memory slot instead of holding each string in the stack
+
 
 # While the game is going: 
 # while not stop_play:
@@ -92,20 +97,54 @@ print("remember, you can always quit the game by entering 'q'")
     # Print next options
 print("Right now you are in the %s and %s" % (room[player1.current_room].name, room[player1.current_room].description))
 
+def prtcurrent(curroom): 
+    print("Right now you are in the %s and %s" % (room[player1.current_room].name, room[player1.current_room].description))
 
 # player inputs direction they want to move in 
     # print if direction is available to the player
         # set current room to this new room
 new_direc = input("Where would you like to go next? North, South, East, or West? Choose Wisely...")
 print(new_direc)
-if room[player1.current_room].new_direc == "empty":
-    print('there is no room there') 
-    # if direction is not available, have them pick another direction
-else:
-    player1.current_room == room[player1.current_room].new_direc
+# if new_direc == 'north':
+#     # if direction is not available, have them pick another direction
+#     if room[player1.current_room].n_to == empty:
+#         print('there is no room there') 
 
+#     # otherwise set the current room to the "n_to" room
+#     else:
+#         new_room = room[player1.current_room].n_to
+#         print(new_room)
+#         print(player1.current_room)
+# Overlook || Treasure
+# Foyer    || Narrow
+# Outside
 
+# if player current room is outside, then north goes to foyer
+if player1.current_room == 'outside':
+    if new_direc == 'north':
+        player1.current_room = 'foyer'
+        prtcurrent(player1.current_room)
+        new_direc = input("Where would you like to go next? North, South, East, or West? Choose Wisely...")
+    else:
+        new_direc = input("Where would you like to go next? North, South, East, or West? Choose Wisely...")
+# if player current room is foyer, then north goes to overlook, east goes to narrow, south goes to outside
+if player1.current_room == 'foyer':
+    if new_direc == 'north':
+        player1.current_room = 'overlook'
+        prtcurrent(player1.current_room)
+    elif new_direc == 'east':
+        player1.current_room = 'narrow'
+        prtcurrent(player1.current_room)
+    elif new_direc == 'south':
+        player1.current_room = 'outside'
+    else:
+        print("You stare at an empty wall... You wonder how long it's been there...")
+        new_direc = input("Where would you like to go next? North, South, East, or West? Choose Wisely...") 
+# player current room is overlook, then east goes to treasure, south goes to foyer
 
+# player current room is treasure, then west goes to overlook, south goes to narrow
+
+# player current room is narow, then west goes to foyer, north goes to outside
 
 
 
